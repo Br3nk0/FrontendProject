7f2b919713fd14e930269068e351f99be82a4e64
Br3nk0 <demeyerebrenko@gmail.com>
2014-12-03 11:07:24 +0100

    Navigatie scroll
    De active class wordt verplaatst naar de juiste a href wanneer je
    scrollt in de pagina

---
+++ Scripts/project.js
@@ -0,0 +1,23 @@
+/**
+ * Created by BrenkoD on 3/12/2014.
+ */
+var divs = $('div')
+    , ul = $('ul')
+    , ul_height = ul.outerHeight();
+
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    divs.each(function() {
+        var top = $(this).offset().top - ul_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            ul.find('a').removeClass('active');
+            divs.removeClass('active');
+
+            $(this).addClass('active');
+            ul.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+        }
+    });
+});
