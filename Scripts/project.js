e6611c944e37f3c261e27fd4ff8e70bdc90402d9
Br3nk0 <demeyerebrenko@gmail.com>
2014-12-23 11:49:05 +0100

    drag en drop game pacman

--- Scripts/project.js
+++ Scripts/project.js
@@ -41,3 +41,17 @@ function onScroll(event){
     });
 }
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("image", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("image");
+    ev.target.appendChild(document.getElementById(data));
+    window.location.href='Pacman.html'
+}
