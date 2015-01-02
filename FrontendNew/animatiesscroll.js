d5a4a0037ca62ae397dcd7a271ee81da9fa94c80
jolenefouquaert <jolenefouquaert@gmail.com>
2015-01-02 22:53:23 +0100

    angular css
    nog enkele foutjes

--- FrontendNew/animatiesscroll.js
+++ FrontendNew/animatiesscroll.js
@@ -15,3 +15,18 @@ $(window).scroll(function() {
     });
 });
 
+$(window).scroll(function() {
+    $('.naam').each(function(){
+        var imagePos = $(this).offset().top;
+        var winH = $(window).height();
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow > winH ) {
+            console.log("nu slide about in")
+            $(this).addClass("fadeIn");
+        }
+        else {
+            $(this).removeClass("fadeIn");
+        }
+
+    });
+});
